eff48a1af047c15e14036e1211d51b10b6bb7828
siphokazi <usphow@gmail.com>
2023-11-29 12:51:23 -0800

    insert left

--- 1-binary_insert_left.c
+++ 1-binary_insert_left.c
@@ -8,27 +8,22 @@
  * Return: If an error occurs or if parent is NULL - NULL.
  *         Otherwise - a pointer to the created node.
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
 	new_node = binary_tree_node(parent, value);
-
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-
 	parent->left = new_node;
 
 	return (new_node);
